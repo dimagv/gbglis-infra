7f4760c4877e3fa122dd21a96ecdf2edf3a6dbe1
dimag <dmitry.gvl@gmail.com>
2019-08-20 21:21:23 +0300

    asdf

--- up.sh
+++ up.sh
@@ -4,5 +4,5 @@ BRANCH=$1
 
 echo "up branch: ${BRANCH}"
 
-cd /home/ironjab/gbglis
+cd /home/ironjab/gbgliscicd
 git clone -b ${BRANCH} --single-branch http://192.168.160.166:8080/tfs/DMDL/_git/GBGLIS ${BRANCH}
