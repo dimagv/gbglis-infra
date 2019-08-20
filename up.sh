4bb55ff0d22653e2623a90f7e50b0eb8581b9dce
dimag <dmitry.gvl@gmail.com>
2019-08-20 21:46:49 +0300

    asdf

--- up.sh
+++ up.sh
@@ -6,8 +6,8 @@ PASSWORD=$3
 
 
 echo "up branch: ${BRANCH}"
-echo $USERNAME
-echo $PASSWORD
+echo "username: ${USERNAME}"
+echo "password: ${PASSWORD}"
 
 cd /home/ironjab/gbgliscicd
 git clone -b ${BRANCH} --single-branch http://${USERNAME}:${PASSWORD}@192.168.160.166:8080/tfs/DMDL/_git/GBGLIS ${BRANCH}
