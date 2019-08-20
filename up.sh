288df285413413e30eb9c44692914cb01374cdae
dimag <dmitry.gvl@gmail.com>
2019-08-20 23:07:02 +0300

    asdf

--- up.sh
+++ up.sh
@@ -4,14 +4,45 @@ BRANCH=$1
 USERNAME=$2
 PAT=$3
 
+JENKINS_URL="http://192.168.161.240:8320"
+BASE_URL="http://192.168.160.166:8080/tfs/DMDL/"
+REPO_URL=$BASE_URL+"_git/GBGLIS"
+HOOK_URL=$BASE_URL+"_apis/hooks/subscriptions"
+
 
 echo "up branch: ${BRANCH}"
 echo "username: ${USERNAME}"
 echo "password: ${PAT}"
 
-cd /home/ironjab/gbgliscicd
-git -c http.extraheader="AUTHORIZATION: Basic $(echo -n $USERNAME:$PAT |base64 -w0)" clone -b $BRANCH --single-branch http://192.168.160.166:8080/tfs/DMDL/_git/GBGLIS $BRANCH
+service_hook_data() {
+  cat <<EOF
+{
+  "publisherId": "tfs",
+  "eventType": "git.push",
+  "consumerId": "jenkins",
+  "consumerActionId": "triggerGenericBuild",
+  "publisherInputs": {
+    "branch": $BRANCH,
+    "projectId": "327ebaa7-1ccd-48e3-90d5-96f8caa03664",
+    "repository": "5fd70a75-7cc0-4596-9089-a84966188769",
+  },
+  "consumerInputs": {
+    "buildName": "GBGLIS/$BRANCH",
+    "serverBaseUrl": $JENKINS_URL,
+    "username": "admin",
+    "useTfsPlugin": "built-in",
+    "password": "admin"
+  },
+}
+EOF
+}
 
-cd $BRANCH
+curl -i -H "Accept: application/json; api-version=1.0" -H "Content-Type:application/json" --data "$(service_hook_data)" -XPOST -u :$PAT $HOOK_URL
 
-ls -la
+
+
+
+#cd /home/ironjab/gbgliscicd
+#git -c http.extraheader="AUTHORIZATION: Basic $(echo -n $USERNAME:$PAT |base64 -w0)" clone -b $BRANCH --single-branch $REPO_URL $BRANCH
+#cd $BRANCH
+#ls -la
