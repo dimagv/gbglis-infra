1e7691bb7dd2cb2e18261c35f09a76886f0b10a1
dimag <dmitry.gvl@gmail.com>
2019-08-23 01:39:03 +0300

    asdf

--- down.sh
+++ down.sh
@@ -35,13 +35,13 @@ echo "[1] OK"
 # [2] REMOVE NGINX CONFS
 #############################################################################################
 echo "[2] Removing nginx confs for: $BRANCH"
-echo "[2] Removing WEB config"
+echo "[2] Removing WEB config: $NGINX_DIR/$BRANCH.$DOMAIN.conf"
 [ -f $NGINX_DIR/$BRANCH.$DOMAIN.conf ] && rm $NGINX_DIR/$BRANCH.$DOMAIN.conf
-echo "[2] Removing API config"
+echo "[2] Removing API config: $NGINX_DIR/$BRANCH-api.$DOMAIN.conf"
 [ -f $NGINX_DIR/$BRANCH-api.$DOMAIN.conf ] && rm $NGINX_DIR/$BRANCH-api.$DOMAIN.conf
 echo "[2] Removing IDENTITY config"
 [ -f $NGINX_DIR/$BRANCH-identity.$DOMAIN.conf ] && rm $NGINX_DIR/$BRANCH-identity.$DOMAIN.conf
-echo "[2] Restarting nginx"
+echo "[2] Restarting nginx: $NGINX_DIR/$BRANCH-identity.$DOMAIN.conf"
 docker kill -s HUP $NGINX_CONTAINER_NAME
 echo "[2] OK"
 #############################################################################################
@@ -55,7 +55,7 @@ apt update -qq && apt install -y -qq jq
 remove_hook() {
     local HOOK_ID=$1
     echo "Removing hook: $HOOK_ID"
-    STATUS_CODE=`curl -XDELETE --write-out %{http_code} --silent --output /dev/null -u :$TFS_TOKEN $HOOK_URL/$HOOK_ID`
+    STATUS_CODE=`curl -XDELETE -H "Accept: api-version=1.0" --write-out %{http_code} --silent --output /dev/null -u :$TFS_TOKEN $HOOK_URL/$HOOK_ID`
     if [[ $STATUS_CODE -eq 203 ]]; then
             echo "Hook '$HOOK_ID' successfully removed"
     else
