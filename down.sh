7e53cbfc71194747201d62d07c8b43e50103fca0
dimag <dmitry.gvl@gmail.com>
2019-08-22 23:56:39 +0300

    asdf

--- down.sh
+++ down.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "down branch: $1"
+BRANCH=$1
+DOMAIN=$2
+TFS_USER=$3
+TFS_TOKEN=$4
+
+GBGLIS_DIR="/home/ironjab/gbglis/$BRANCH"
+NGINX_DIR="/home/ironjab/nginx/conf.d"
+NGINX_CONTAINER_NAME="global-nginx"
+BASE_URL="http://192.168.160.166:8080/tfs/DMDL/"
+HOOK_URL="${BASE_URL}_apis/hooks/subscriptions"
 
 # 1 cd branch dir
 # 2 docker-compose down
@@ -10,3 +19,49 @@ echo "down branch: $1"
 #   4.2 search by .publisherInputs.branch
 #   4.3 remove
 # 5 remove nginx conf / restart
+
+echo "[0] DOWN START"
+
+# [1] STOP/REMOVE APP
+#############################################################################################
+# echo "[1] Stopping app for: $BRANCH"
+
+# if [ -d $GBGLIS_DIR ]; then
+#     cd $GBGLIS_DIR
+#     echo "[1] running docker-compose down, pwd: $PWD"
+#     docker-compose down
+#     echo "[1] Removing app dir: $GBGLIS_DIR"
+#     rm -rf $GBGLIS_DIR
+# else
+#     echo "[1] Skipped. Dir: $GBGLIS_DIR does not exists"
+# fi
+
+# echo "[1] OK"
+#############################################################################################
+
+# [2] REMOVE NGINX CONFS
+#############################################################################################
+# echo "[2] Removing nginx confs for: $BRANCH"
+# echo "[2] Removing WEB config"
+# [ -f $NGINX_DIR/$BRANCH.$DOMAIN.conf ] && rm $NGINX_DIR/$BRANCH.$DOMAIN.conf
+# echo "[2] Removing API config"
+# [ -f $NGINX_DIR/$BRANCH-api.$DOMAIN.conf ] && rm $NGINX_DIR/$BRANCH-api.$DOMAIN.conf
+# echo "[2] Removing IDENTITY config"
+# [ -f $NGINX_DIR/$BRANCH-identity.$DOMAIN.conf ] && rm $NGINX_DIR/$BRANCH-identity.$DOMAIN.conf
+# echo "[2] Restarting nginx"
+# docker kill -s HUP $NGINX_CONTAINER_NAME
+# echo "[2] OK"
+#############################################################################################
+
+# [3] REMOVE TFS SERVICE HOOKS
+#############################################################################################
+echo "[3] Removing TFS service hooks for: $BRANCH"
+
+HOOKS=$(curl -s -H "Accept: application/json; api-version=1.0" -H "Content-Type:application/json" -XGET -u :$TFS_TOKEN $HOOK_URL | jq -c '.value[] | select(.publisherInputs.branch | contains($BRANCH)) | .id')
+echo "HOOKS: $HOOKS"
+
+
+echo "[3] OK"
+#############################################################################################
+
+echo "[0] DOWN DONE"
