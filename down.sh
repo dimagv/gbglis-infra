49b5701584487fa324f6ea27e520777c3c2d16ba
dimag <dmitry.gvl@gmail.com>
2019-08-23 01:59:55 +0300

    asdf

--- down.sh
+++ down.sh
@@ -57,6 +57,7 @@ remove_hook() {
     echo "Removing hook: $HOOK_ID"
     # STATUS_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" -XDELETE -H "Accept: api-version=1.0" -u :$TFS_TOKEN $HOOK_URL/$HOOK_ID)
     echo $HOOK_URL/$HOOK_ID
+    echo $TFS_TOKEN
     curl -XDELETE -H "Accept: api-version=1.0" -u :$TFS_TOKEN $HOOK_URL/$HOOK_ID
     if [[ $STATUS_CODE -eq 204 ]]; then
             echo "Hook '$HOOK_ID' successfully removed"
