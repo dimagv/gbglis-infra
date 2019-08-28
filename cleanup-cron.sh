fa225d0180ea42047e2b2dd8b0f57b2457972e66
dimag <dmitry.gvl@gmail.com>
2019-08-28 23:50:09 +0300

    asdf

--- cleanup-cron.sh
+++ cleanup-cron.sh
@@ -8,7 +8,7 @@ TFS_TOKEN=$3
 INFRA_REPO=$4
 INFRA_DIR="/tmp/cleanup"
 
-echo "[cleanup-cron] START"
+echo "[cleanup-cron] START: $(date) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 echo "[cleanup-cron] Clonning infra repo '$INFRA_REPO' to '$INFRA_DIR'"
 git clone $INFRA_REPO $INFRA_DIR
@@ -19,4 +19,4 @@ sudo /tmp/cleanup/cleanup.sh $DOMAIN $TFS_USER $TFS_TOKEN
 echo "[cleanup-cron] Removing infra dir: $INFRA_DIR"
 rm -rf $INFRA_DIR
 
-echo "[cleanup-cron] DONE"
+echo "[cleanup-cron] DONE: $(date) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
