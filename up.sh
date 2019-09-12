c5ba6fe8087748f97ff770a2753807cbc26cdef5
dimag <dmitry.gvl@gmail.com>
2019-09-12 21:02:03 +0300

    asdf

--- up.sh
+++ up.sh
@@ -2,19 +2,7 @@
 
 set -e
 
-urlencode() {
-    # urlencode <string>
-    local length="${#1}"
-    for (( i = 0; i < length; i++ )); do
-        local c="${1:i:1}"
-        case $c in
-            [a-zA-Z0-9.~_-]) printf "$c" ;;
-            *) printf '%%%02X' "'$c"
-        esac
-    done
-}
-
-BRANCH=$(urlencode $1)
+BRANCH=$1
 DOMAIN=$2
 TFS_USER=$3
 TFS_TOKEN=$4
